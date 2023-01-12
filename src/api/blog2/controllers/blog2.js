5b59e7eafabfae0f2c5e3948b6f0ec481f2680d7
Jasson Caceres <caceres@MBP-de-Jasson.lan>
2023-01-11 23:36:10 -0500

    adding back-end with strapi

---
+++ src/api/blog2/controllers/blog2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog2 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog2.blog2');
---
+++ src/api/blog2/routes/blog2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog2 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blog2.blog2');
